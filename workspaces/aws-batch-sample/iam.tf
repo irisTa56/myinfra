795092c0b7f91399cc60fb88f0db1063dbfd22af
irisTa56 <takayuki.kobayashi@access-company.com>
2021-10-17 16:36:19 +0900

    update iam for aws batch

--- workspaces/aws-batch-sample/iam.tf
+++ workspaces/aws-batch-sample/iam.tf
@@ -1,9 +1,41 @@
 resource "aws_iam_role" "aws_batch_service_role" {
   name               = "aws_batch_service_role"
-  assume_role_policy = file("${path.module}/policy/aws_batch_service_role.json")
+  assume_role_policy = data.aws_iam_policy_document.batch.json
+}
+
+data "aws_iam_policy_document" "batch" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   role       = aws_iam_role.aws_batch_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "tf_test_batch_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks.json
+}
+
+data "aws_iam_policy_document" "ecs_tasks" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
